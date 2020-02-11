fca0718526f60e08a2850d682d57f2d25273aeae
Ida Laasasenaho <ida.laasasenaho@gmail.com>
2020-02-11 15:48:27 +0200

    updates

--- src/game.js
+++ src/game.js
@@ -1,4 +1,9 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
+const minLimit = 1,
+    maxLimit = 100;
+const maxGuessCount = 100;
+
+let randomNumber = Math.floor(Math.random() * maxLimit) + minLimit;
+console.log('random number', randomNumber);
 
 const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
@@ -6,14 +11,68 @@ const lowOrHi = document.querySelector('.lowOrHi');
 
 const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
+const resultText = document.querySelector('.resultText');
 
 let guessCount = 1;
+let startTime;
 let resetButton;
 
-const checkGuess = () => {
-    let userGuess = Number(guessField.value);
+
+const resetGame = () => {
+    guessCount = 1;
+
+    const resetParas = document.querySelectorAll('.resultParas p');
+
+    for (const p of resetParas) {
+        p.textContent = '';
+    }
+
+    resetButton.parentNode.removeChild(resetButton);
+
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
+
+    lastResult.style.backgroundColor = 'white';
+
+    randomNumber = Math.floor(Math.random() * maxLimit) + minLimit;
+    console.log('random number', randomNumber);
+};
+
+const getResultText = (startTime, endTime, numberOfGuesses) => {
+    if (numberOfGuesses === 1) {
+        return 'Damn, you are a one lucky --beep--!';
+    }
+    return `It took ${((endTime - startTime)/1000)} seconds and
+          ${numberOfGuesses} tries to guess it right.`;
+};
+
+const setGameOver = () => {
+    resultText.textContent = getResultText(startTime, Date.now(), guessCount);
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start new game';
+    document.body.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
+};
+
+
+const checkGuess = (input) => {
+    // console.log('checkguess input', input);
+    let userGuess;
+    // -1: too low, 0: correct, 1: too high
+    let guessDirection;
+    if (typeof input === 'object') {
+        userGuess = Number(guessField.value);
+    } else {
+        userGuess = input;
+    }
+
     if (guessCount === 1) {
         guesses.textContent = 'Previous guesses: ';
+        startTime = Date.now();
     }
     guesses.textContent += userGuess + ' ';
 
@@ -22,7 +81,8 @@ const checkGuess = () => {
         lastResult.style.backgroundColor = 'green';
         lowOrHi.textContent = '';
         setGameOver();
-    } else if (guessCount === 10) {
+        return 0;
+    } else if (guessCount === maxGuessCount) {
         lastResult.textContent = '!!!GAME OVER!!!';
         setGameOver();
     } else {
@@ -30,12 +90,20 @@ const checkGuess = () => {
         lastResult.style.backgroundColor = 'red';
         if (userGuess < randomNumber) {
             lowOrHi.textContent = 'Last guess was too low!';
+            return -1;
         } else if (userGuess > randomNumber) {
             lowOrHi.textContent = 'Last guess was too high!';
+            return 1;
         }
     }
 
     guessCount++;
     guessField.value = '';
     guessField.focus();
-}
+};
+
+const StartGame = () => {
+    guessSubmit.addEventListener('click', checkGuess);
+};
+
+export { StartGame, checkGuess, resetGame };
